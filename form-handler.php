018eb605c1d7049c64005690ba97865b1a4ac6b6
PRANAV ZENDE <pranavzende97@gmail.com>
2026-06-23 01:27:12 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$vistor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'iamjoker982222.com'
+
+$email_subject = 'New Form Submission'
+
+$email_body = "User Name: $name.\n".
+              "User Email: $vistor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'godsniper838@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $vistor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+?>
